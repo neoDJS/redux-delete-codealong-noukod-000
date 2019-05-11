b88ffadd61d83e63af3ff5a6fedaf4e48a59c474
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-11 06:09:12 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -7,8 +7,7 @@ export default function manageTodo(state = {
       return { todos: state.todos.concat(action.payload.text) };
     case 'DELETE_TODO':
 
-      return {todos: state.todos.filter(todo => todo !== action.payload)}
-
+      return {todos: state.todos.filter(todo => todo !== action.payload)};
     default:
       return state;
   }
